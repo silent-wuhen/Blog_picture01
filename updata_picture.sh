322506499aca32977aecdfc61d797a58ebc86e4a
silent-wuhen <3281320387@qq.com>
2025-04-05 02:20:43 +0800

    路径更改第一次验证

--- updata_picture.sh
+++ updata_picture.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# 定义参数
-md_file_path="C:/Users/wuhen/Desktop/商品大数据可视化系统.md"  # 替换为你的 .md 文件路径
-old_image_prefix="https://github.com/silent-wuhen/Blog_picture01/blob/main/3.%E6%96%87%E6%A1%A3%E5%9B%BE%E7%89%87/11.PythonProject/1.%E5%95%86%E5%93%81%E6%95%B0%E6%8D%AE%E5%8F%AF%E8%A7%86%E5%8C%96%E5%88%86%E6%9E%90%E5%A4%A7%E5%B1%8F/"  # 替换为要修改的图片网址前缀
-new_image_prefix="https://cdn.jsdelivr.net/gh/silent-wuhen/Blog_picture01/3.文档图片/11.PythonProject/1.商品数据可视化分析大屏/"  # 替换为新的图片网址前缀
+# 参数1
+md_file_path="D:/study/H01_开发技能/1.Git&Github/1.Git基础/Git基础.md"  # 替换为 .md 文件路径
+# 参数2
+old_image_prefix="https://github.com/silent-wuhen/Blog_picture01/blob/main/3.%E6%96%87%E6%A1%A3%E5%9B%BE%E7%89%87/2.Git"  # 替换为要修改的图片网址前缀
+# 参数3
+new_image_prefix="https://cdn.jsdelivr.net/gh/silent-wuhen/Blog_picture01/H01_开发技能/1.Git&Github/1.Git基础/Git基础"  # 替换为新的图片网址前缀
 
 # 检查文件是否存在
 if [ ! -f "$md_file_path" ]; then
