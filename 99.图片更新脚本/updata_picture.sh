ac362e7d4b4b5a1338f5b825705c9f6f97a46d41
silent-wuhen <3281320387@qq.com>
2025-04-05 15:14:12 +0800

    updata

--- 99.图片更新脚本/updata_picture.sh
+++ 99.图片更新脚本/updata_picture.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # 参数1:替换为 .md 文件路径
-md_file_path="D:\\study\\H03_其他\\2.java\\2.java基础\\Java基础语法.md"
+md_file_path="C:\\Users\\wuhen\\Desktop\\计算机科学速成\\1.Crash Course Computer Science（未完结）.md"
 
 # 参数2:要修改的图片网址前缀
-old_image_prefix="https://github.com/silent-wuhen/Blog_picture01/blob/main/3.%E6%96%87%E6%A1%A3%E5%9B%BE%E7%89%87/9.java/1.java%E8%AF%AD%E6%B3%95"
+old_image_prefix="https://github.com/silent-wuhen/Blog_picture01/blob/main/3.%E6%96%87%E6%A1%A3%E5%9B%BE%E7%89%87/1.%E8%AE%A1%E7%AE%97%E6%9C%BA%E7%A7%91%E5%AD%A6%E9%80%9F%E6%88%90"
 
 # 参数3:替换为新的图片网址前缀
-new_image_prefix="https://cdn.jsdelivr.net/gh/silent-wuhen/Blog_picture01/H03_其他/2.java/2.java基础"
+new_image_prefix="https://cdn.jsdelivr.net/gh/silent-wuhen/Blog_picture01/H02_计算机基础/1.计算机导论/1.计算机科学速成"
 
 echo "File Path: $md_file_path"
 echo "Old Prefix: $old_image_prefix"
