1448d0e5481618a4d99a2ba97c88ecb3b75c4ad5
silent-wuhen <3281320387@qq.com>
2026-03-19 20:50:27 +0800

    updata

---
+++ pull&push.sh
@@ -0,0 +1,10 @@
+echo =============pull=============
+git pull
+sleep 5
+echo =============pull完成=============
+echo =============push=============
+git add .
+git commit -m "updata"
+git push
+echo =============push完成=============
+sleep 5
--- updata.sh
+++
@@ -1,9 +0,0 @@
-echo ===========================
-git pull
-sleep 5
-echo ===========================
-git add .
-git commit -m "updata"
-git push
-echo ===========================
-sleep 5
